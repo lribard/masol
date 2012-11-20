cefa1183854618c5f20b5a72cc126a453460a818
unknown <LR@lr-mac-win7.private.4d.fr>
2012-11-20 10:46:45 +0100

    Initial commit

---
+++ test_stamp_transaction/test.js
@@ -0,0 +1,9 @@
+﻿ds.startTransaction();
+var ent = ds.DataClass1.find('ID = 1');
+var stamp = ent.getStamp();
+ent.attribute1 = ent.attribute1 + 'x';
+ent.save();
+ds.commit();
+
+ent = ds.DataClass1.find('ID = 1');
+var stamp2 = ent.getStamp();
